04d76e1a79dceda58a6deee7252e82896508285d
zhangjx <zhangjx@boostor.com.cn>
2021-01-28 16:55:36 +0800

    添加设置支付

--- app/components/SettingContent/index.tsx
+++ app/components/SettingContent/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import styles from './index.scss';
+import classnames from 'classnames';
+import Tilt from 'react-tilt';
 
+import wechatQRcodeImage from 'assets/qrcode/wechat.png';
+import alipayQRcodeImage from 'assets/qrcode/alipay.png';
+import { ReactComponent as QRcodeIcon } from 'assets/icons/QRcode.svg';
+import { ReactComponent as SettingIcon } from 'assets/icons/setting.svg';
+import { ReactComponent as LinkIcon } from 'assets/icons/link.svg';
+import styles from './index.scss';
+import commonStyles from '../../styles/common.scss';
 export interface SettingContentProps {
   onEnter: () => void;
   onClose: () => void;
 }
 const SettingContent: React.FC<SettingContentProps> = props => {
   return (
-    <div className={styles.content}>
+    <div className={classnames(styles.content)}>
       <div className={styles.header}>
         <button className={styles.close} onClick={props.onClose}>
           关闭
@@ -18,8 +26,52 @@ const SettingContent: React.FC<SettingContentProps> = props => {
         </button>
       </div>
       <div className={styles.body}>
-        
+        <div>
+          <div className={styles.title}>
+            <SettingIcon />
+            <span>通用设置</span>
+          </div>
+          <div></div>
+        </div>
+        <div>
+          <div className={styles.title}>
+            <QRcodeIcon />
+            <span>支付宝支持一下～</span>
+          </div>
+          <div className={styles.pay}>
+            <Tilt
+              options={{ max: 25 }}
+              className={classnames(styles.qrcode, styles.alipay)}
+            >
+              <img src={alipayQRcodeImage} />
+            </Tilt>
+          </div>
+        </div>
+        <div>
+          <div className={styles.title}>
+            <QRcodeIcon />
+            <span>微信支持一下～</span>
+          </div>
+          <div className={styles.pay}>
+            <Tilt
+              options={{ max: 25 }}
+              className={classnames(styles.qrcode, styles.wechat)}
+            >
+              <img src={wechatQRcodeImage} />
+            </Tilt>
+          </div>
+        </div>
+        <div>
+          <div className={styles.title}>
+            <LinkIcon />
+            <span>联系作者</span>
+          </div>
+          <div className={styles.link}>
+            <a>https://github.com/1zilc/fishing-funds</a>
+          </div>
+        </div>
       </div>
+      <div className={styles.version}>version 1.0.0</div>
     </div>
   );
 };
