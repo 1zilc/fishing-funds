563f6a7ad12350dfef4626865b5947a698c6d1e2
1zilc <dywzzjx@163.com>
2022-05-03 17:43:56 +0800

    还原collapse逻辑

--- src/renderer/components/Collapse/index.tsx
+++ src/renderer/components/Collapse/index.tsx
@@ -10,12 +10,20 @@ interface CollapseProps {
 
 const Collapse: React.FC<PropsWithChildren<CollapseProps>> = (props) => {
   const contentRef = useRef<HTMLDivElement>(null);
+  const [maxHeight, setMaxHeight] = useState(contentRef.current?.clientHeight);
+
+  /**
+   * 在渲染前取到最新的字组件高度，设置为collapse最大高度
+   */
+  useLayoutEffect(() => {
+    setMaxHeight(contentRef.current?.clientHeight);
+  }, [contentRef.current?.clientHeight]);
 
   return (
     <div
       className={clsx(styles.content)}
       style={{
-        maxHeight: props.isOpened ? contentRef.current?.clientHeight : 0,
+        maxHeight: props.isOpened ? maxHeight : 0,
         ...props.style,
       }}
     >
