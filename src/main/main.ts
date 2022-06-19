93aa3c1be280aca354f6be058ac7b9df48af74d3
1zilc <dywzzjx@163.com>
2022-06-19 22:03:54 +0800

    6.1.1 (#467)
    * 修复快捷键bug
    
    * 修改版本号6.1.1
    
    * 修复快捷键设置

--- src/main/main.ts
+++ src/main/main.ts
@@ -128,11 +128,13 @@ function main() {
     touchBarManager.updateEysStatusItems(config);
   });
   ipcMain.handle('set-hotkey', (event, keys: string) => {
-    if (!keys) {
-      globalShortcut.unregister(activeHotkeys);
-    } else if (activeHotkeys === keys) {
-    } else {
-      activeHotkeys = keys;
+    if (keys === activeHotkeys) {
+      return;
+    }
+    if (activeHotkeys) {
+      globalShortcut.unregister(activeHotkeys.split(' + ').join('+'));
+    }
+    if (keys) {
       const accelerator = keys.split(' + ').join('+');
       const ret = globalShortcut.register(accelerator, () => {
         const isWindowVisible = mb.window?.isVisible();
@@ -153,6 +155,7 @@ function main() {
         }
       }
     }
+    activeHotkeys = keys;
   });
   // menubar 相关监听
   mb.on('after-create-window', () => {
