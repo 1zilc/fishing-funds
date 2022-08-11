182bc29a4a796ed059ee64e9685d899e58379067
1zilc <44715458+1zilc@users.noreply.github.com>
2022-08-11 12:39:48 +0800

    修改main:pro配置

--- .erb/configs/webpack.config.main.prod.ts
+++ .erb/configs/webpack.config.main.prod.ts
@@ -43,30 +43,6 @@ const configuration: webpack.Configuration = {
     minimizer: [new ESBuildMinifyPlugin()],
   },
 
-  module: {
-    rules: [
-      {
-        test: /\.worker\.ts$/,
-        use: {
-          loader: 'worker-loader',
-          options: {
-            filename: '[name].js',
-            inline: 'fallback',
-          },
-        },
-      },
-      {
-        test: /\.[jt]sx?$/,
-        exclude: /node_modules/,
-        loader: 'esbuild-loader',
-        options: {
-          loader: 'tsx',
-          target: 'esnext',
-        },
-      },
-    ],
-  },
-
   plugins: [
     new BundleAnalyzerPlugin({
       analyzerMode: process.env.ANALYZE === 'true' ? 'server' : 'disabled',
