9e0bf688d1d6353c1b68eee44235dda4d3d09712
1zilc <44715458+1zilc@users.noreply.github.com>
2022-10-09 20:28:49 +0800

    fix: 删除多余asset

--- src/renderer/typings/typings.d.ts
+++ src/renderer/typings/typings.d.ts
@@ -4,7 +4,3 @@ declare module '*.png';
 declare module '*.svg' {
   export default function ReactComponent(props: React.SVGProps<SVGSVGElement>): React.ReactElement;
 }
-declare module '*.svg?url' {
-  const content: string;
-  export default content;
-}
