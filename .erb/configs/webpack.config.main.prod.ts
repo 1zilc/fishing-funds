3aee163f2330a415030e738c793a4fddb8238437
1zilc <44715458+1zilc@users.noreply.github.com>
2022-10-13 09:34:23 +0800

    chore: fix entry main

--- .erb/configs/webpack.config.main.prod.ts
+++ .erb/configs/webpack.config.main.prod.ts
@@ -30,7 +30,7 @@ const configuration: webpack.Configuration = {
   target: 'electron-main',
 
   entry: {
-    main: [path.join(webpackPaths.srcMainPath, 'main.ts')],
+    main: path.join(webpackPaths.srcMainPath, 'main.ts'),
     preload: path.join(webpackPaths.srcMainPath, 'preload.ts'),
   },
 
