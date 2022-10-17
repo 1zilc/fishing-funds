b92d1c2dacd04dac6fc1ec41152db02927e37b8c
1zilc <44715458+1zilc@users.noreply.github.com>
2022-10-17 11:22:38 +0800

    fix: 优化globalStyle

--- src/renderer/components/GlobalStyles/index.tsx
+++ src/renderer/components/GlobalStyles/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { useAppSelector } from '@/utils/hooks';
+import { useAppSelector, useThemeColor } from '@/utils/hooks';
 import * as Enums from '@/utils/enums';
 
 interface GlobalStylesProps {}
 
 const GlobalStyles: React.FC<GlobalStylesProps> = () => {
-  const { lowKeySetting, baseFontSizeSetting, themeColorTypeSetting, customThemeColorSetting } = useAppSelector(
-    (state) => state.setting.systemSetting
-  );
-  const customThemeColorEnable = themeColorTypeSetting === Enums.ThemeColorType.Custom;
+  const { lowKeySetting, baseFontSizeSetting } = useAppSelector((state) => state.setting.systemSetting);
+  const { customThemeColorEnable, customThemeColorSetting } = useThemeColor();
 
   return (
     <>
