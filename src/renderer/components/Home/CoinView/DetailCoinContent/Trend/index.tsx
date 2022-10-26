6fe3c916539a62c4dc7570ea9708ea9dbc97c5b3
1zilc <44715458+1zilc@users.noreply.github.com>
2022-10-26 21:14:38 +0800

    fix: 修改货币交易量统计

--- src/renderer/components/Home/CoinView/DetailCoinContent/Trend/index.tsx
+++ src/renderer/components/Home/CoinView/DetailCoinContent/Trend/index.tsx
@@ -26,7 +26,7 @@ const dateTypeList = [
 
 const Trend: React.FC<PerformanceProps> = ({ code, name }) => {
   const { ref: chartRef, chartInstance } = useResizeEchart(CONST.DEFAULT.ECHARTS_SCALE);
-  const [date, setDateType] = useState(dateTypeList[2]);
+  const [date, setDateType] = useState(dateTypeList[1]);
   const coinUnitSetting = useAppSelector((state) => state.setting.systemSetting.coinUnitSetting);
 
   const { data: result = { prices: [], vol24h: [] }, run: runGetHistoryFromCoingecko } = useRequest(
@@ -109,10 +109,8 @@ const Trend: React.FC<PerformanceProps> = ({ code, name }) => {
           {
             data: result.vol24h.map(({ time, price }) => [time, price]),
             yAxisIndex: 1,
-            type: 'line',
+            type: 'bar',
             name: `24H交易量 (亿)`,
-            showSymbol: false,
-            symbol: 'none',
             smooth: true,
           },
         ],
