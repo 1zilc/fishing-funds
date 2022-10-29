8969eff10e465fa282121749f6338ea947d2e4ba
1zilc <44715458+1zilc@users.noreply.github.com>
2022-10-29 19:18:19 +0800

    fix: 修复基金导入bug

--- src/renderer/utils/hooks/system.ts
+++ src/renderer/utils/hooks/system.ts
@@ -170,7 +170,7 @@ export function useFundsClipboard() {
   useIpcRendererListener('clipboard-funds-import', async (e: Electron.IpcRendererEvent, data) => {
     try {
       const limit = 1024;
-      const text = clipboard.readText();
+      const text = await clipboard.readText();
       const json: any[] = JSON.parse(text);
       if (json.length > limit) {
         dialog.showMessageBox({
