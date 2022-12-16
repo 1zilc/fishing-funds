aa0bbb466136950dc19ad6e489690048f576e44d
1zilc <44715458+1zilc@users.noreply.github.com>
2022-12-16 23:25:38 +0800

    fix: 修复接口类型

--- src/renderer/services/stock.ts
+++ src/renderer/services/stock.ts
@@ -63,22 +63,6 @@ export async function SearchFromEastmoney(keyword: string) {
         cb: 'cb',
       },
     });
-    type Result = {
-      Type: number; // 7,
-      Name: string; // "三板",
-      Count: number; // 3;
-      Datas: {
-        Code: string; // '839489';
-        Name: string; // '同步天成';
-        ID: string; // '839489_TB';
-        MktNum: string; // '0';
-        SecurityType: string; // '10';
-        MarketType: string; // '_TB';
-        JYS: string; // '81';
-        GubaCode: string; // '839489';
-        UnifiedCode: string; // '839489';
-      }[];
-    }[];
     const data = eval(script);
 
     return (
@@ -101,7 +85,7 @@ export async function SearchFromEastmoney(keyword: string) {
             };
           }),
         };
-      }) as Result
+      }) as Stock.SearchResult[]
     ).filter((item) => !!item.Count);
   } catch (error) {
     return [];
