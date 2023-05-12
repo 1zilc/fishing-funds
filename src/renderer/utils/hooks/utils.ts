159167103bbe69d559fa8de41dcc9dd21f54bd6c
1zilc <44715458+1zilc@users.noreply.github.com>
2023-05-12 23:13:53 +0800

    revert: chart resise

--- src/renderer/utils/hooks/utils.ts
+++ src/renderer/utils/hooks/utils.ts
@@ -1,7 +1,7 @@
-import { useLayoutEffect, useState, useEffect, useRef, useMemo, startTransition } from 'react';
+import React, { useLayoutEffect, useState, useEffect, useRef, useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 import queryString from 'query-string';
-import { useInterval, useBoolean, useThrottleFn, useSize, useMemoizedFn, useEventListener, useUpdateEffect } from 'ahooks';
+import { useInterval, useBoolean, useThrottleFn, useSize, useMemoizedFn, useEventListener } from 'ahooks';
 import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
 import dayjs from 'dayjs';
 import * as echarts from 'echarts';
@@ -91,10 +91,10 @@ export function useScrollToTop(config: {
 }
 
 export function useResizeEchart(scale = 1, unlimited?: boolean) {
-  const sizeInited = useRef(false);
   const chartRef = useRef<HTMLDivElement>(null);
   const chartInstanceRef = useRef<echarts.ECharts>();
   const chartWidth = useSize(chartRef)?.width;
+  // const chartWidth = useDeferredValue(size?.width);
 
   useEffect(() => {
     const instance = echarts.init(chartRef.current!, undefined, {
@@ -106,24 +106,19 @@ export function useResizeEchart(scale = 1, unlimited?: boolean) {
     };
   }, []);
 
-  useUpdateEffect(() => {
-    if (sizeInited.current) {
-      startTransition(() => {
-        if (chartWidth) {
-          const height = chartWidth * scale;
-          chartInstanceRef.current?.resize({ height: unlimited ? height : Math.min(height, 240) });
-        }
-      });
-    } else {
-      sizeInited.current = true;
-    }
-  }, [chartWidth]);
+  useAfterMountedEffect(() => {
+    React.startTransition(() => {
+      if (chartWidth) {
+        const height = chartWidth * scale;
+        chartInstanceRef.current?.resize({ height: unlimited ? height : height > 200 ? 200 : height });
+      }
+    });
+  }, [chartWidth, unlimited]);
 
   return { ref: chartRef, chartInstance: chartInstanceRef.current, chartInstanceRef };
 }
 
 export function useAutoSizeEchart() {
-  const sizeInited = useRef(false);
   const chartRef = useRef<HTMLDivElement>(null);
   const chartInstanceRef = useRef<echarts.ECharts>();
   const chartWidth = useSize(chartRef)?.width;
@@ -139,16 +134,12 @@ export function useAutoSizeEchart() {
     };
   }, []);
 
-  useUpdateEffect(() => {
-    if (sizeInited.current) {
-      startTransition(() => {
-        if (chartWidth && chartHeight) {
-          chartInstanceRef.current?.resize();
-        }
-      });
-    } else {
-      sizeInited.current = true;
-    }
+  useAfterMountedEffect(() => {
+    React.startTransition(() => {
+      if (chartWidth && chartHeight) {
+        chartInstanceRef.current?.resize();
+      }
+    });
   }, [chartWidth, chartHeight]);
 
   return { ref: chartRef, chartInstance: chartInstanceRef.current, chartInstanceRef };
