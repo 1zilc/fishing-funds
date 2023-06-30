74b3376bf97fe1531328ec5613b6d74ec2f553c2
1zilc <44715458+1zilc@users.noreply.github.com>
2023-06-30 11:23:34 +0800

    feat: 优化打包

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,35 +3,39 @@ import mkcert from 'vite-plugin-mkcert';
 import react from '@vitejs/plugin-react';
 import { defineConfig, splitVendorChunkPlugin } from 'electron-vite';
 
-// electron.vite.config.js
-export default defineConfig({
-  main: {
-    // vite config options
-    build: {
-      outDir: 'release/app/dist/main',
-    },
-  },
-  preload: {
-    // vite config options
-    build: {
-      outDir: 'release/app/dist/preload',
-    },
-  },
-  renderer: {
-    // vite config options
-    build: {
-      outDir: 'release/app/dist/renderer',
-    },
-    server: {
-      port: 3456,
-      strictPort: true,
-      https: true,
-    },
-    plugins: [react(), mkcert(), splitVendorChunkPlugin()],
-    resolve: {
-      alias: {
-        '@': path.resolve('src/renderer'),
+export default defineConfig(({ command }) => {
+  const prod = command === 'build';
+
+  return {
+    main: {
+      build: {
+        outDir: 'release/app/dist/main',
+        minify: prod,
       },
     },
-  },
+    preload: {
+      build: {
+        outDir: 'release/app/dist/preload',
+        minify: prod,
+      },
+    },
+    renderer: {
+      build: {
+        outDir: 'release/app/dist/renderer',
+        minify: prod,
+        cssCodeSplit: false,
+      },
+      server: {
+        port: 3456,
+        strictPort: true,
+        https: true,
+      },
+      plugins: [react(), mkcert(), splitVendorChunkPlugin()],
+      resolve: {
+        alias: {
+          '@': path.resolve('src/renderer'),
+        },
+      },
+    },
+  };
 });
