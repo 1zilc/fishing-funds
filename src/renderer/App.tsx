84504d9476b749de3a668ce38bec28c5fc9c78a2
1zilc <44715458+1zilc@users.noreply.github.com>
2023-06-30 10:37:19 +0800

    fix: 路由

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import LoadingScreen from '@/components/LoadingScreen';
 import ThemeProvider from '@/components/ThemeProvider';
 import { useAppSelector, useThemeColor } from '@/utils/hooks';
@@ -16,6 +16,50 @@ const DetailQuotationPage = React.lazy(() => import('@/components/Home/Quotation
 const DetailCoinPage = React.lazy(() => import('@/components/Home/CoinView/DetailCoinPage'));
 const WebViewerPage = React.lazy(() => import('@/components/WebViewerDrawer/WebViewerPage'));
 
+const router = createMemoryRouter(
+  [
+    {
+      path: CONST.ROUTES.INIT,
+      element: <InitPage />,
+    },
+    {
+      path: CONST.ROUTES.HOME,
+      element: <HomePage />,
+    },
+    {
+      path: CONST.ROUTES.DETAIL,
+      element: <DetailPage />,
+      children: [
+        {
+          path: CONST.ROUTES.DETAIL_FUND,
+          element: <DetailFundPage />,
+        },
+        {
+          path: CONST.ROUTES.DETAIL_ZINDEX,
+          element: <DetailZindexPage />,
+        },
+        {
+          path: CONST.ROUTES.DETAIL_QUOTATION,
+          element: <DetailQuotationPage />,
+        },
+        {
+          path: CONST.ROUTES.DETAIL_STOCK,
+          element: <DetailStockPage />,
+        },
+        {
+          path: CONST.ROUTES.DETAIL_COIN,
+          element: <DetailCoinPage />,
+        },
+        {
+          path: CONST.ROUTES.DETAIL_WEBVIEWER,
+          element: <WebViewerPage />,
+        },
+      ],
+    },
+  ],
+  { initialEntries: ['/'] }
+);
+
 const App: React.FC<Record<string, unknown>> = () => {
   const darkMode = useAppSelector((state) => state.setting.darkMode);
   const baseFontSizeSetting = useAppSelector((state) => state.setting.systemSetting.baseFontSizeSetting);
@@ -31,22 +75,9 @@ const App: React.FC<Record<string, unknown>> = () => {
         primaryColor: customThemeColorEnable ? customThemeColorSetting || originPrimaryColor : originPrimaryColor,
       }}
     >
-      <Router>
-        <Suspense fallback={<LoadingScreen loading text="请稍后..." />}>
-          <Routes>
-            <Route path={CONST.ROUTES.INIT} element={<InitPage />} />
-            <Route path={CONST.ROUTES.HOME} element={<HomePage />} />
-            <Route path={CONST.ROUTES.DETAIL} element={<DetailPage />}>
-              <Route path={CONST.ROUTES.DETAIL_FUND} element={<DetailFundPage />} />
-              <Route path={CONST.ROUTES.DETAIL_ZINDEX} element={<DetailZindexPage />} />
-              <Route path={CONST.ROUTES.DETAIL_QUOTATION} element={<DetailQuotationPage />} />
-              <Route path={CONST.ROUTES.DETAIL_STOCK} element={<DetailStockPage />} />
-              <Route path={CONST.ROUTES.DETAIL_COIN} element={<DetailCoinPage />} />
-              <Route path={CONST.ROUTES.DETAIL_WEBVIEWER} element={<WebViewerPage />} />
-            </Route>
-          </Routes>
-        </Suspense>
-      </Router>
+      <Suspense fallback={<LoadingScreen loading text="请稍后..." />}>
+        <RouterProvider router={router} />
+      </Suspense>
     </ThemeProvider>
   );
 };
