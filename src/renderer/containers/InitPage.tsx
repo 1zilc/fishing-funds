8ace15df23681130852f6a747215eee7cb8beaac
1zilc <44715458+1zilc@users.noreply.github.com>
2023-07-04 09:19:56 +0800

    fix: 优化钱包显示

--- src/renderer/containers/InitPage.tsx
+++ src/renderer/containers/InitPage.tsx
@@ -125,7 +125,7 @@ const InitPage = () => {
     // 视图配置加载完成
     dispatch(setViewModeAction(allStateStorage[CONST.STORAGE.VIEW_MODE] || sortInitialState.viewMode));
     // 保密状态加载完成
-    dispatch(syncEyeStatusAction(allStateStorage[CONST.STORAGE.EYE_STATUS] || Enums.EyeStatus.Open));
+    dispatch(syncEyeStatusAction(allStateStorage[CONST.STORAGE.EYE_STATUS] || true));
     /**
      * cache部分
      */
