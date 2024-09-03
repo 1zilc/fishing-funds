87b0169d78f11465449964db74127bfcebded2f5
1zilc <44715458+1zilc@users.noreply.github.com>
2024-09-03 20:34:15 +0800

    💄 style: 调整颜色哈希参数

--- src/renderer/utils/colorHash.ts
+++ src/renderer/utils/colorHash.ts
@@ -1,6 +1,3 @@
 import ColorHash from 'color-hash';
 
-export default new ColorHash({
-  // lightness: 0.6,
-  // saturation: 0.72,
-});
+export default new ColorHash({ saturation: 0.72, lightness: 0.65 });
