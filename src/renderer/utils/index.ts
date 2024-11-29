d7380038af4d177a8da354d3e942bcdde30c74b6
1zilc <44715458+1zilc@users.noreply.github.com>
2024-11-29 22:25:12 +0800

    ♻️ refactor(utils): 优化静态资源导入
    - 将 import.meta.url 赋值给 metaUrl 变量，提高代码可读性
    - 使用 metaUrl 变量替换 ImportStatic 函数中的 import.meta.url，避免重复代码

--- src/renderer/utils/index.ts
+++ src/renderer/utils/index.ts
@@ -4,6 +4,8 @@ import Color from 'color';
 import dayjs from 'dayjs';
 import * as CONST from '@/constants';
 
+const metaUrl = import.meta.url;
+
 export function Yang(num: string | number | undefined) {
   try {
     if (num === undefined) {
@@ -435,5 +437,5 @@ export function MergeStateWithResponse<C, CK extends keyof C, SK extends keyof S
 }
 
 export function ImportStatic(path: string) {
-  return new URL(`../static/${path}`, import.meta.url).href;
+  return new URL(`../static/${path}`, metaUrl).href;
 }
