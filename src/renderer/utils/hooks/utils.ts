eccbaa4101c3de459fe315747dad8492292eba6e
1zilc <44715458+1zilc@users.noreply.github.com>
2025-09-03 18:35:59 +0800

    V8.6.1 (#725)
    * Revert "chore: 移除所有memo"
    
    This reverts commit c714f3efe150cd1832c7b55573fc4e49cfdcc0b7.
    
    * chore: 移除所有memo
    
    * 🐛 fix(quotation-view): 修复 react 导入语法
      - 移除 react 导入语句中多余的逗号
    
    * 📦 chore: 更新版本号至8.6.1
    
    本次提交将应用版本号从8.6.0更新至8.6.1，为下一个发布版本做准备。这是一个简单的版本号更新操作，不涉及功能变更或代码修改。
    
    * ♻️ refactor: 修复 openWebView 参数类型错误
    
    将 openWebView 函数的参数类型从数组改为单个参数，使其能够正确处理字符串或对象类型的 URL 参数。这个修改解决了之前只能传递数组参数的问题，使函数调用更加灵活和直观。

--- src/renderer/utils/hooks/utils.ts
+++ src/renderer/utils/hooks/utils.ts
@@ -2,7 +2,7 @@ import React, { useLayoutEffect, useState, useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 import { useRequest } from 'ahooks';
-import { useInterval, useBoolean, useThrottleFn, useSize, useEventListener } from 'ahooks';
+import { useInterval, useBoolean, useThrottleFn, useSize, useMemoizedFn, useEventListener } from 'ahooks';
 import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux';
 import dayjs from 'dayjs';
 import * as echarts from 'echarts/core';
@@ -688,7 +688,7 @@ export function useFundConfigMap(codes: string[]) {
 }
 
 export function useIpcRendererListener(channel: string, listener: (event: Electron.IpcRendererEvent, ...args: any[]) => void) {
-  const callback = listener;
+  const callback = useMemoizedFn(listener);
 
   useEffect(() => {
     ipcRenderer.on(channel, callback);
